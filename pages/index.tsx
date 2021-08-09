5e58559353c3ecae030147ec6ae447ddd52339ca
Jyotiranjan Sahoo <jsahoo.p@gmail.com>
2021-08-09 17:59:07 +0530

    added typescript support

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,26 +4,30 @@ import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import Date from "../components/date";
+import { GetStaticProps } from "next";
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
-
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
+}) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello my name is Jyotiranjan Sahoo. I'm a software developer</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
+          Hello my name is Jyotiranjan Sahoo and I'm a Software Developer. You
+          can reach out to me on{" "}
+          <a href="https://www.linkedin.com/in/jyotiranjan-sahoo/">LinkedIn</a>
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this in{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
@@ -46,3 +50,12 @@ export default function Home({ allPostsData }) {
     </Layout>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+};
